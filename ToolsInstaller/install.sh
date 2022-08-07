a405b148f4369a24e29728ef626e9f1d5b81f932
Ricardo <ricardofps66@gmail.com>
2022-08-07 14:38:29 -0300

    Adding new tool to install.sh

--- ToolsInstaller/install.sh
+++ ToolsInstaller/install.sh
@@ -69,6 +69,14 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
+echo "Baixando Termius..."
+
+wget https://autoupdate.termius.com/linux/Termius.deb -P ~/Downloads/
+
+echo "Instalando Termius"
+
+dpkg -i ~/Downloads/Termius.deb
+
 echo "Baixando Wireshark..."
 
 sudo add-apt-repository ppa:wireshark-dev/stable
