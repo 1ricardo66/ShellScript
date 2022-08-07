b578965dfd9eafb93f3f02f7d4ba55b8c3457517
Ricardo <ricardofps66@gmail.com>
2022-08-07 14:33:01 -0300

    fix winbox.sh && Updating install.sh

--- ToolsInstaller/install.sh
+++ ToolsInstaller/install.sh
@@ -63,6 +63,12 @@ echo "Instalando VS code"
 
 sudo dpkg -i ~/Downloads/code_1.70.0-1659589288_amd64.deb
 
+echo "Baixando Spotify..."
+
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
 echo "Baixando Wireshark..."
 
 sudo add-apt-repository ppa:wireshark-dev/stable
